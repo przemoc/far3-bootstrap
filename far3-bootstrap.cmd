7e96029b0b27fa6764d0a573f53c4ccc412869b4
Przemyslaw Pawelczyk <przemoc@gmail.com>
2021-10-24 20:09:01 +0200

    Pass arguments from far3-bootstrap.cmd to far3-bootstrap.sh.

--- far3-bootstrap.cmd
+++ far3-bootstrap.cmd
@@ -4,4 +4,4 @@ set "download=certutil -URLcache -split -f"
 echo Downloading busybox.exe from '%busybox%'
 %download% %busybox% busybox.exe
 echo Executing far3-bootstrap.sh using busybox sh
-busybox sh far3-bootstrap.sh
+busybox sh far3-bootstrap.sh %*
