cb03ab7b0ecebe9db1352a1e67e76687e687f620
Przemyslaw Pawelczyk <przemoc@gmail.com>
2023-06-17 19:10:49 +0200

    Support running far3-bootstrap.ps1 from different directory.

--- far3-bootstrap.ps1
+++ far3-bootstrap.ps1
@@ -2,4 +2,4 @@ $busybox = "http://frippery.org/files/busybox/busybox.exe"
 "Downloading busybox.exe from $busybox"
 Invoke-WebRequest -Uri $busybox -OutFile busybox.exe
 "Executing far3-bootstrap.sh using busybox sh"
-.\busybox sh far3-bootstrap.sh $args
+.\busybox sh $PSScriptRoot\far3-bootstrap.sh $args
